98c919aa45f65477acd3793c2e673ab87d6532de
Minras <andrey.shchurkov@gmail.com>
2018-10-08 01:23:21 +0200

    #2 Add the copyright text

--- app/src/main/java/com/minras/lovesyou/CopyrightActivity.java
+++ app/src/main/java/com/minras/lovesyou/CopyrightActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.widget.TextView;
 
 /**
  * Created by Andrii Shchurkov on 10/7/2018.
@@ -19,5 +21,8 @@ public class CopyrightActivity extends AppCompatActivity {
         // Get a support ActionBar corresponding to this toolbar
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
+
+        TextView txt = (TextView) findViewById(R.id.text_copyright);
+        txt.setText(Html.fromHtml("The unicorn icons made by <a href='https://www.flaticon.com/authors/freepik'>Freepik</a> from <a href='https://www.flaticon.com'>www.flaticon.com</a>. Thank you, guys!"));
     }
 }
