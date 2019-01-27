71df037d285eaaaaa2d7c03c7acb1d45a0c4370f
Minras <andrey.shchurkov@gmail.com>
2019-01-27 22:13:00 +0100

    #2 Added time-based quotes

--- app/src/main/java/com/minras/lovesyou/ua/MainActivity.java
+++ app/src/main/java/com/minras/lovesyou/ua/MainActivity.java
@@ -13,8 +13,10 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-import org.apache.commons.lang3.ArrayUtils;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
 import java.util.Random;
 
 import static com.minras.lovesyou.ua.Config.INTENT_DATA_KEY_SETTINGS;
@@ -23,10 +25,14 @@ import static com.minras.lovesyou.ua.Config.SHARED_PREFERENCES_NAME;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    final private String PREFIX_ASK = "ask";
+    final private String PREFIX_TELL = "tell";
+
     private int largeIconsTreshold = 2000;
     private String loveText = "";
     private TextView loveTextView;
 
+    private String pkg;
     private SharedPreferences sharedPreferences;
     private Settings settings = new Settings();
 
@@ -35,6 +41,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        pkg = getPackageName();
         sharedPreferences = getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -57,17 +64,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             btnAsk.setImageResource(R.drawable.unicorn_256_1049947);
         }
 
-        showMessage(R.array.tell_array, R.array.tell_array_personal);
+        showMessage(PREFIX_TELL);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.askButton:
-                showMessage(R.array.ask_array, R.array.ask_array_personal);
+                showMessage(PREFIX_ASK);
                 break;
             case R.id.tellButton:
-                showMessage(R.array.tell_array, R.array.tell_array_personal);
+                showMessage(PREFIX_TELL);
                 break;
         }
     }
@@ -112,14 +119,53 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    private void showMessage(int dataId, int personalDataId) {
-        String[] array;
-        if ("ol.zvereva@gmail.com".equals(settings.getAccountEmail()) || "andrey.shchurkov@gmail.com".equals(settings.getAccountEmail())) {
-            array = ArrayUtils.addAll(getResources().getStringArray(dataId), getResources().getStringArray(personalDataId));
-        } else {
-            array = getResources().getStringArray(dataId);
+    private boolean isMorning(final int hour) {
+        return hour >= 5 && hour <= 11;
+    }
+
+    private boolean isDay(final int hour) {
+        return hour >= 12 && hour <= 17;
+    }
+
+    private boolean isNight(final int hour) {
+        return (hour == 23) || (hour >= 0 && hour <= 3);
+    }
+
+    private boolean isWinter(final int month) {
+        return month == 0 || month == 1 || month == 11;
+    }
+
+    private List<String> getTimeBasedQuotes(final String prefix) {
+        int resId;
+        List<String> list = new ArrayList<>();
+        int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        int month = Calendar.getInstance().get(Calendar.MONTH);
+        if (isMorning(hour)) {
+            resId = getResources().getIdentifier(prefix + "_array_morning", "array", pkg);
+            list.addAll(Arrays.asList(getResources().getStringArray(resId)));
+        } else if (isDay(hour)) {
+            resId = getResources().getIdentifier(prefix + "_array_day", "array", pkg);
+            list.addAll(Arrays.asList(getResources().getStringArray(resId)));
+        } else if (isNight(hour)) {
+            resId = getResources().getIdentifier(prefix + "_array_night", "array", pkg);
+            list.addAll(Arrays.asList(getResources().getStringArray(resId)));
         }
-        loveText = array[new Random().nextInt(array.length)];
+        if (isWinter(month)) {
+            resId = getResources().getIdentifier(prefix + "_array_winter", "array", pkg);
+            list.addAll(Arrays.asList(getResources().getStringArray(resId)));
+        }
+        return list;
+    }
+
+    private void showMessage(final String prefix) {
+        int resId = getResources().getIdentifier(prefix + "_array", "array", pkg);
+        List<String> list = new ArrayList<String>(Arrays.asList(getResources().getStringArray(resId)));
+        if ("ol.zvereva@gmail.com".equals(settings.getAccountEmail()) || "andrey.shchurkov@gmail.com".equals(settings.getAccountEmail())) {
+            resId = getResources().getIdentifier(prefix + "_array_personal", "array", pkg);
+            list.addAll(Arrays.asList(getResources().getStringArray(resId)));
+        }
+        list.addAll(getTimeBasedQuotes(prefix));
+        loveText = list.get(new Random().nextInt(list.size()));
         loveTextView.setText(loveText);
     }
 }
